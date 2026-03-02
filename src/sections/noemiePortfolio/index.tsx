996072a28576936d4f7b61328d48d2c7bd8c3248
louis <ldescotes1@gmail.com>
2026-03-02 22:37:24 +0100

    test video

--- src/sections/noemiePortfolio/index.tsx
+++ src/sections/noemiePortfolio/index.tsx
@@ -8,55 +8,77 @@ const NoemiePortfolioSection = () => {
       <article className="noemie-portfolio-info">
         <div className="noemie-portfolio-info-title">
           <h2>Noémie portfolio</h2>
-          <div>
-            <a href="https://gaurois.vercel.app/">
-              https://gaurois.vercel.app/
-            </a>
-            <ArrowUpRight />
-          </div>
+          <a
+            className="noemie-portfolio-info-title-link"
+            href="https://gaurois.vercel.app/"
+          >
+            https://gaurois.vercel.app/
+            <ArrowUpRight color="#FC6605" />
+          </a>
         </div>
-        <div>
-          <p>description</p>
+        <div className="noemie-portfolio-info-description">
+          <p className="noemie-portfolio-info-description-title">description</p>
           <p>react, threejs, webgl, motion, scss</p>
         </div>
-        <div>
-          <p>stack</p>
+        <div className="noemie-portfolio-info-description">
+          <p className="noemie-portfolio-info-description-title">stack</p>
           <p>
             Réalisation d’un portfolio pour la photographe indépendante Noémie
             Gaurois, en mettant un focus sur l’expérience utilisateur afin de
             créer des émotions comme le créer ses photos
           </p>
         </div>
-        <div>
-          <p>date</p>
+        <div className="noemie-portfolio-info-description">
+          <p className="noemie-portfolio-info-description-title">date</p>
           <p>2025</p>
         </div>
       </article>
-      <aside className="noemie-portfolio-images">
-        <Image
-          width={740}
-          height={450}
-          src="/test.png"
-          alt="noemie portfolio"
-        />
-        <Image
-          width={740}
-          height={450}
-          src="/test.png"
-          alt="noemie portfolio"
-        />
-        <Image
-          width={740}
-          height={450}
-          src="/test.png"
-          alt="noemie portfolio"
-        />
-        <Image
-          width={740}
-          height={450}
-          src="/test.png"
-          alt="noemie portfolio"
-        />
+      <aside className="noemie-portfolio-presentation">
+        <video
+          width="740"
+          height="450"
+          controls
+          preload="metadata"
+          autoPlay
+          muted
+          loop
+          playsInline
+        >
+          <source
+            src="/projects/noemiePortfolio/noemiePortfolio-intro.mp4"
+            type="video/mp4"
+          />
+        </video>
+        <video
+          width="740"
+          height="450"
+          controls
+          preload="metadata"
+          autoPlay
+          muted
+          loop
+          playsInline
+        >
+          <source
+            src="/projects/noemiePortfolio/noemiePortfolio-scroll.mp4"
+            type="video/mp4"
+          />
+        </video>
+        <video
+          width="740"
+          height="450"
+          controls
+          preload="metadata"
+          autoPlay
+          muted
+          loop
+          playsInline
+        >
+          <source
+            src="/projects/noemiePortfolio/noemiePortfolio-enterProject.mp4"
+            type="video/mp4"
+          />
+        </video>
       </aside>
     </section>
   );
