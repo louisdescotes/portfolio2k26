a7b017d1d42a9cc0cc2d27fefd45434bcfd3f968
louis <ldescotes1@gmail.com>
2026-03-11 21:11:57 +0100

    initial

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  transpilePackages: ['three'],
+  transpilePackages: ["three"],
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      use: ["raw-loader", "glslify-loader"],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
--- src/app/pages/landing/index.tsx
+++ src/app/pages/landing/index.tsx
@@ -1,46 +1,8 @@
-"use client";
-
-import { Canvas } from "@react-three/fiber";
-import "./style.scss";
-import PlaneImage from "@/src/components/planeImage";
-
 const Landing = () => {
   return (
-    <>
-      <div className="landing grid-p">
-        <h1>
-          LOUIS <br />
-          DESCOTES
-        </h1>
-        <p>
-          Adipisicing laborum quis labore. Laboris occaecat consectetur veniam
-          sit exercitation id irure ea culpa exercitation ullamco. Minim aliquip
-          aliqua tempor exercitation dolore nostrud dolor. Id magna sint
-          deserunt occaecat aliqua. Eiusmod adipisicing laborum adipisicing
-          voluptate non officia commodo nisi tempor veniam nisi id laborum
-          veniam.
-        </p>
-      </div>
-      <div id="canvas-container">
-        <Canvas>
-          <PlaneImage />
-          <ambientLight intensity={Math.PI / 2} />
-          <spotLight
-            position={[10, 10, 10]}
-            angle={0.15}
-            penumbra={1}
-            decay={0}
-            intensity={Math.PI}
-          />
-          <pointLight
-            position={[-10, -10, -10]}
-            decay={0}
-            intensity={Math.PI}
-          />
-        </Canvas>
-      </div>
-    </>
+    <section>
+      <h1>LOUIS DESCOTES</h1>
+    </section>
   );
 };
-
 export default Landing;
