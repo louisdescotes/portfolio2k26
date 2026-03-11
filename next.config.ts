947e4648115cb00ba57354c9ce4d02481f7a21cb
louis <ldescotes1@gmail.com>
2026-03-11 21:13:46 +0100

    fix

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   transpilePackages: ["three"],
-
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.(glsl|vs|fs|vert|frag)$/,
-      use: ["raw-loader", "glslify-loader"],
-    });
-
-    return config;
-  },
 };
 
 export default nextConfig;
